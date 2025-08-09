3d611dbd7d30716292fed3423af8b35e93505ae1
Trey Hardin <hello@trumancreative.co>
2025-08-09 15:32:36 -0400

    CF hack

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,5 +23,14 @@ export default defineConfig({
     },
     imageService: 'cloudflare',
   }),
-  output: 'static'
+  output: 'static',
+  vite: {
+    resolve: {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      alias: import.meta.env.PROD && {
+        "react-dom/server": "react-dom/server.edge",
+      },
+    },
+  },
 });
